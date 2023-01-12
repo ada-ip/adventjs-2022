7e460a0cddd62b2090041002659e9f2547116ccf
ada-ip <80456993+ada-ip@users.noreply.github.com>
2023-01-12 13:42:53 +0100

    Add files via upload

--- reto1/reto1.js
+++ reto1/reto1.js
@@ -1,1 +1,25 @@
+const gifts = ["cat", "game", "socks"];
+const wrapped = wrapping(gifts);
 
+console.log(wrapped);
+
+function wrapping(gifts) {
+	// Se envuelve cada regalo y se guarda en un array auxiliar
+	const giftsWrapped = gifts.map((gift) => {
+		let giftWrapped = "";
+		// Se colocan los asteriscos de encima de la palabra
+		for (let i = 0; i < gift.length + 2; i++) {
+			giftWrapped += "*";
+		}
+		// Se colocan los saltos de línea y los asteriscos de los laterales
+		giftWrapped += "\n*" + gift + "*\n";
+		// Se colocan los asteriscos de debajo de la palabra
+		for (let i = 0; i < gift.length + 2; i++) {
+			giftWrapped += "*";
+		}
+
+		return giftWrapped;
+	});
+
+	return giftsWrapped;
+}
