765786a8ea986fc34166513ca798e9f874896671
ada-ip <80456993+ada-ip@users.noreply.github.com>
2023-01-13 09:56:15 +0100

    Add files via upload

--- reto3/reto3.js
+++ reto3/reto3.js
@@ -1,1 +1,22 @@
+const packOfGifts = ["book", "doll", "ball"];
+const reindeers = ["dasher", "dancer"];
 
+// el pack de regalos pesa 4 + 4 + 4 = 12
+// los renos pueden llevar (2 * 6) + (2 * 6) = 24
+// por lo tanto, Santa Claus puede entregar 2 cajas de regalos
+
+distributeGifts(packOfGifts, reindeers); // 2
+
+function distributeGifts(packOfGifts, reindeers) {
+	let giftsWeight = 0;
+	for (let gift of packOfGifts) {
+		giftsWeight += gift.length;
+	}
+
+	let reindeersWeight = 0;
+	for (let reindeer of reindeers) {
+		reindeersWeight += reindeer.length * 2;
+	}
+
+	return Math.floor(reindeersWeight / giftsWeight);
+}
