3f9fde7adb5f5f8e368013923bb0925e12fc8e9b
ada-ip <80456993+ada-ip@users.noreply.github.com>
2023-01-16 09:01:43 +0100

    Add files via upload

--- reto4/reto4.js
+++ reto4/reto4.js
@@ -1,1 +1,30 @@
+let boxes = [
+	{ l: 1, w: 1, h: 1 },
+	{ l: 2, w: 2, h: 2 },
+	{ l: 3, w: 1, h: 3 }
+];
 
+fitsInOneBox(boxes); // false
+
+boxes = [
+	{ l: 1, w: 1, h: 1 },
+	{ l: 3, w: 3, h: 3 },
+	{ l: 2, w: 2, h: 2 }
+];
+
+fitsInOneBox(boxes); // true
+
+function fitsInOneBox(boxes) {
+	let boxesFit = true;
+
+	boxes.sort((box1, box2) => box1.l - box2.l);
+
+	// Se comparan los tamaños de las cajas
+	for (let i = 1; i < boxes.length && boxesFit; i++) {
+		if (boxes[i - 1].l >= boxes[i].l || boxes[i - 1].w >= boxes[i].w || boxes[i - 1].h >= boxes[i].h) {
+			boxesFit = false;
+		}
+	}
+
+	return boxesFit;
+}
