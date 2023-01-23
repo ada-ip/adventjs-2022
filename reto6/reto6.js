7d206ba2b63eb3f3bb4dfa233d6cb47d19c2d4bf
ada-ip <80456993+ada-ip@users.noreply.github.com>
2023-01-23 09:46:49 +0100

    Add files via upload

--- reto6/reto6.js
+++ reto6/reto6.js
@@ -1,1 +1,47 @@
+function createCube(size) {
+	let cube = "";
 
+	// Se crea la mitad superior del cubo
+	for (let i = 1; i <= size; i++) {
+		// Espacios iniciales
+		for (let j = 1; j <= size - i; j++) {
+			cube += " ";
+		}
+
+		// String de la cara 1
+		for (let j = 1; j <= i; j++) {
+			cube += "/\\";
+		}
+
+		// String de la cara 2
+		for (let j = 1; j <= size; j++) {
+			cube += "_\\";
+		}
+
+		cube += "\n";
+	}
+
+	// Se crea la mitad inferior del cubo
+	for (let i = size; i >= 1; i--) {
+		// Espacios iniciales
+		for (let j = 1; j <= size - i; j++) {
+			cube += " ";
+		}
+
+		// String de la cara 1
+		for (let j = 1; j <= i; j++) {
+			cube += "\\/";
+		}
+
+		// String de la cara 3
+		for (let j = 1; j <= size; j++) {
+			cube += "_/";
+		}
+
+		if (i > 1) {
+			cube += "\n";
+		}
+	}
+
+	return cube;
+}
