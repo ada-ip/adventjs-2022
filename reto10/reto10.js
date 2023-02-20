db3f992f86c0f2c7666e81af0ca318c285e3d9eb
ada-ip <80456993+ada-ip@users.noreply.github.com>
2023-02-20 08:59:22 +0100

    Add files via upload

--- reto10/reto10.js
+++ reto10/reto10.js
@@ -1,1 +1,28 @@
+function checkJump(heights) {
+	let sube = 0;
+	let baja = 0;
+	let estado;
+	let parabola = true;
 
+	for (let i = 1; i < heights.length && parabola; i++) {
+		if (heights[i] > heights[i - 1] && estado != "sube") {
+			sube++;
+			estado = "sube";
+		}
+
+		if (heights[i] < heights[i - 1] && estado != "baja") {
+			baja++;
+			estado = "baja";
+		}
+
+		if (baja > sube || sube > 1 || baja > 1) {
+			parabola = false;
+		}
+	}
+
+	if (sube != 1 || baja != 1) {
+		parabola = false;
+	}
+
+	return parabola;
+}
